51d179110090181dc2ae1e3757d104b4240d6d6b
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2025-07-14 20:21:27 -0400

    move main.go into cmd/server directory

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log/slog"
 	"net"
 )
+
+func handleRequest(conn net.Conn) {
+}
 
 func main() {
 	listener, err := net.Listen("tcp", ":8080")
@@ -20,7 +22,8 @@ func main() {
 			slog.Error("Failed to accept new connection: " + err.Error())
 		}
 		slog.Info("Accepted new connection from " + conn.RemoteAddr().String())
-		err = sendMessage(conn)
+		// err = sendMessage(conn)
+		go handleRequest(conn)
 		if err != nil {
 			slog.Error("Failed to send message to client: " + err.Error())
 		}
@@ -29,13 +32,13 @@ func main() {
 	}
 }
 
-func sendMessage(conn net.Conn) error {
-	slog.Info("Sending message to client")
-	message := "Hello, user!\n"
-	n, err := conn.Write([]byte(message))
-	if err != nil {
-		return err
-	}
-	slog.Info("Wrote " + fmt.Sprint(n) + " bytes to client")
-	return nil
-}
+// func sendMessage(conn net.Conn) error {
+// 	slog.Info("Sending message to client")
+// 	message := "Hello, user!\n"
+// 	n, err := conn.Write([]byte(message))
+// 	if err != nil {
+// 		return err
+// 	}
+// 	slog.Info("Wrote " + fmt.Sprint(n) + " bytes to client")
+// 	return nil
+// }
