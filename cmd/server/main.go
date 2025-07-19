c4c88d7396477fee7d77d97ed918a0a2ac356f0a
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2025-07-19 15:10:35 -0400

    parse request

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,39 +1,37 @@
 package main
 
 import (
-	"log/slog"
+	"fmt"
 	"net"
+	"os"
 
 	"github.com/theaaronruss/go-http-server/internal"
 )
 
 func handleRequest(conn net.Conn) {
 	defer conn.Close()
-	slog.Info("Handling request for client")
 	request, err := request.ReadRequest(conn)
 	if err != nil {
-		slog.Error(err.Error())
+		fmt.Fprintln(os.Stderr, err.Error())
 	}
-	slog.Info("Parsed request", "request", request)
+	fmt.Println("Parsed request:", *request)
 }
 
 func main() {
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
-		slog.Error("Failed to create listener: " + err.Error())
+		fmt.Fprintln(os.Stderr, "Failed to create listener:", err.Error())
 		return
 	}
 	defer listener.Close()
-	slog.Info("Server started (" + listener.Addr().String() + ")")
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			slog.Error("Failed to accept new connection: " + err.Error())
+			fmt.Fprintln(os.Stderr, "Failed to accept new connection:", err.Error())
 		}
-		slog.Info("Accepted new connection from " + conn.RemoteAddr().String())
 		go handleRequest(conn)
 		if err != nil {
-			slog.Error("Failed to send message to client: " + err.Error())
+			fmt.Fprintln(os.Stderr, "Failed to send message to client:", err.Error())
 		}
 	}
 }
--- internal/request.go
+++ internal/request.go
@@ -1,8 +1,11 @@
 package request
 
 import (
+	"errors"
 	"fmt"
 	"net"
+	"slices"
+	"strings"
 )
 
 const initialBufferLen = 8
@@ -18,7 +21,7 @@ const (
 
 type Request struct {
 	Method        string
-	RequestTarget string
+	Target string
 	HttpVersion   string
 	Headers       map[string]string
 	Body          []byte
@@ -33,7 +36,7 @@ func ReadRequest(conn net.Conn) (*Request, error) {
 	}
 	buffer := make([]byte, initialBufferLen)
 	readBytes := 0
-	// parsedBytes := 0
+	parsedBytes := 0
 	for request.state != requestStateDone {
 		if readBytes >= len(buffer) {
 			newBuffer := make([]byte, len(buffer) * 2)
@@ -45,9 +48,72 @@ func ReadRequest(conn net.Conn) (*Request, error) {
 			return nil, fmt.Errorf("failed to read request: %w", err)
 		}
 		readBytes += n
+		p := -1
+		for p != 0 {
+			p, err = request.parse(buffer[parsedBytes:readBytes])
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse request: %w", err)
+			}
+			parsedBytes += p
+		}
 	}
-	return nil, nil
+	return request, nil
 }
+
+func (request *Request) parse(data []byte) (int, error) {
+	switch request.state {
+	case requestStateStatusLine:
+		return request.parseRequestLine(data)
+	case requestStateHeaders:
+		return request.parseHeader(data)
+	default:
+		return 0, nil
+	}
+}
+
+func (request *Request) parseRequestLine(data []byte) (int, error) {
+	dataStr := string(data)
+	lineEndIndex := strings.Index(dataStr, "\r\n")
+	if lineEndIndex == -1 {
+		return 0, nil
+	}
+	parts := strings.Split(dataStr[:lineEndIndex], " ")
+	if len(parts) != 3 {
+		return 0, errors.New("invalid request line")
+	}
+	method := strings.ToUpper(parts[0])
+	if !slices.Contains(httpMethods, method) {
+		return 0, errors.New("invalid http method")
+	}
+	target := parts[1]
+	version := parts[2]
+	request.Method = method
+	request.Target = target
+	request.HttpVersion = version
+	request.state = requestStateHeaders
+	return lineEndIndex + 2, nil
+}
+
+func (request *Request) parseHeader(data []byte) (int, error) {
+	dataStr := string(data)
+	lineEndIndex := strings.Index(dataStr, "\r\n")
+	if lineEndIndex == -1 {
+		return 0, nil
+	}
+	if lineEndIndex == 0 {
+		request.state = requestStateDone
+		return 2, nil
+	}
+	colonIndex := strings.Index(dataStr, ":")
+	if colonIndex == -1 {
+		return 0, errors.New("invalid header")
+	}
+	headerName := strings.ToLower(dataStr[:colonIndex])
+	headerValue := strings.TrimSpace(dataStr[colonIndex + 1:lineEndIndex])
+	request.Headers[headerName] = headerValue
+	return lineEndIndex + 2, nil
+}
+
 
 
 
